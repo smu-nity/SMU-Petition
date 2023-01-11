0244c698c530c409656e5d80e84c2229ea88b2af
hyunmin0317 <choihm9903@naver.com>
2023-01-11 23:19:40 +0900

    feat: core 앱 세팅

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', views.home, name='index'),
 ]
