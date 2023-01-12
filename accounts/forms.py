1a2e6cb2832690ac0cec7a1ef3e1b763d4e0972e
rlJzr <eopy0928@naver.com>
2023-01-13 04:49:23 +0900

    회원가입 form 추가 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,3 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class UserForm(UserCreationForm):
+    email = forms.EmailFiled(label="이메일")
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")
