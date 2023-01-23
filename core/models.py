5a29c3284d1a7e5ceb7aeace1a76d114a09ff8a6
오윤 <oyun@oyun-ui-MacBookAir.local>
2023-01-23 19:50:31 +0900

    feat_issue_14

--- core/models.py
+++ core/models.py
@@ -11,3 +11,5 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+
--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from core.models import Question, Answer
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
+
 
 
 def home(request):
@@ -21,7 +24,33 @@ def detail(request, question_id):
 
 
 def answer_create(request, question_id):
+
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('core:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('core:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'core/question_detail.html', context)
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    # return redirect('core:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('core:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'core/question_form.html', context)
