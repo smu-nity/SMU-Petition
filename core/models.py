1c089943b137c7701a05d5f4994bece08ece8827
rlJzr <eopy0928@naver.com>
2023-01-31 10:34:22 +0900

    청원 동의기능 추가

--- core/models.py
+++ core/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
 
 class Answer(models.Model):
