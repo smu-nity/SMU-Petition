b3c34184a7e055875efdae7e5956c489682f234f
hyunmin0317 <choihm9903@naver.com>
2023-02-05 11:24:40 +0900

    feat: 답변 추천 (#17)

--- core/urls.py
+++ core/urls.py
@@ -6,13 +6,14 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
-    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
-    path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
-    path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
 
     path('question/create/', views.question_create, name='question_create'),
     path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
-    path('question/delete/<int:question_id>/', views.question_delete, name='question_delete')
+    path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),
+    path('question/vote/<int:question_id>/', views.question_vote, name='question_vote'),
 
-
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
+    path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
+    path('answer/vote/<int:answer_id>/', views.answer_vote, name='answer_vote'),
 ]
