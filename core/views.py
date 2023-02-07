1b89f7beb9f852b5473c01ce405d4769a706bfa6
hyunmin0317 <choihm9903@naver.com>
2023-02-07 19:41:26 +0900

    feat: 정렬 - 추천순 (#34)

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
@@ -13,8 +14,8 @@ def home(request):
 
 
 def index(request):
-    sort_dic = {'0': '-create_date', '1': 'create_date'}
-    question_list = Question.objects.all()
+    sort_dic = {'0': '-create_date', '1': 'create_date', '2': '-voter_count'}
+    question_list = Question.objects.all().annotate(voter_count=Count('voter'))
     category = request.GET.get('category', '0')
     sort = request.GET.get('sort', '0')
     if category != '0':
