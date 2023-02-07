ac49b231ed959e7e14a1c2d169f451456d6e54ef
오윤 <oyun@oyun-ui-MacBookAir.local>
2023-02-07 17:10:37 +0900

    feat_issue_27

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from core.models import Question, Answer
@@ -36,7 +36,8 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('core:detail', question_id=question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('core:detail', question_id=question.id), answer.id))
     else:
         return HttpResponseNotAllowed('Only POST is possible.')
     context = {'question': question, 'form': form}
@@ -110,7 +111,8 @@ def answer_modify(request, answer_id):
             answer = form.save(commit=False)
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('core:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('core:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
@@ -131,7 +133,8 @@ def answer_delete(request, answer_id):
 def answer_vote(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.user == answer.author:
-        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
     else:
         answer.voter.add(request.user)
-    return redirect('core:detail', question_id=answer.question.id)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('core:detail', question_id=answer.question.id), answer.id))
