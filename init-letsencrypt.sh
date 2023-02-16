469174a95ac95e01f7e18f2f06095f85ab432182
hyunmin0317 <choihm9903@naver.com>
2023-02-16 17:22:27 +0900

    chore: HTTPS 인증 (#48)

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(smunity.co.kr www.smunity.co.kr)
+domains=(smu-petition.com www.smu-petition.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="choihm9903@gmail.com" # Adding a valid address is strongly recommended
