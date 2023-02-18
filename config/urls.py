5095f74a78a3e9e4d2d92893d576b2d03297d120
hyunmin0317 <choihm9903@naver.com>
2023-02-18 11:32:13 +0900

    chore: robots.txt 추가 (#63)

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from petitions import views
 
 
@@ -23,5 +25,5 @@ urlpatterns = [
     path('petitions/', include('petitions.urls')),
     path('accounts/', include('accounts.urls')),
     path('', views.home, name='home'),
-
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
