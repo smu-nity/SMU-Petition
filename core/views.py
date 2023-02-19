157b8928cb8337db97577f2b4d1c1eb222470f28
rlJzr <eopy0928@naver.com>
2023-02-19 13:22:16 +0900

    댓글페이징기능 추가완료

--- core/views.py
+++ core/views.py
@@ -33,11 +33,15 @@ def petition_list(request):
 
 def petition_detail(request, petition_id):
     petition = get_object_or_404(Petition, pk=petition_id)
+    comment_list = Comment.objects.all().annotate(author_count=Count('author'))
     context = {'petition': petition}
     answers = Answer.objects.filter(petition=petition)
+    page = request.GET.get('page', '1')
     if answers:
         context['answer'] = answers.first()
-    return render(request, 'core/petition_detail.html', context)
+    paginator = Paginator(comment_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    return render(request, 'core/petition_detail.html', context, {'petition_detail': page_obj})
 
 
 @login_required
