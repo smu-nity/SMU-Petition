1887370adc1da116fdf8d6de9e6eeb64dde89746
hyunmin0317 <choihm9903@naver.com>
2023-08-15 22:58:28 +0900

    fix: django-crontab 설정 (#149)

--- petitions/cron.py
+++ petitions/cron.py
@@ -1,5 +1,10 @@
+import os
+import sys
 from datetime import datetime
-
+from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + '/app')))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+application = get_wsgi_application()
 from petitions.models import Petition
 
 
