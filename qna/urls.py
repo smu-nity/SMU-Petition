09b16c5a5748f9baacb711e583dda2bca5252666
hyunmin0317 <choihm9903@naver.com>
2023-08-28 22:41:27 +0900

    feat: 질문 게시판 CRUD (#215)

--- qna/urls.py
+++ qna/urls.py
@@ -7,4 +7,8 @@ app_name = 'qna'
 urlpatterns = [
     path('', views.question_list, name='question_list'),
     path('<int:question_id>/', views.question_detail, name='question_detail'),
+    path('create/', views.question_create, name='question_create'),
+    path('modify/<int:question_id>/', views.question_modify, name='question_modify'),
+    path('delete/<int:question_id>/', views.question_delete, name='question_delete'),
+
 ]
--- qna/views.py
+++ qna/views.py
@@ -1,10 +1,16 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
-from qna.models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from petitions.decorators import superuser_required
+from qna.forms import QuestionForm
+from qna.models import Question, Answer
 
 
 def question_list(request):
-    ql = Question.objects.all()
+    ql = Question.objects.all().order_by('-create_date')
     page = request.GET.get('page', '1')
     paginator = Paginator(ql, 5)
     page_obj = paginator.get_page(page)
@@ -13,4 +19,75 @@ def question_list(request):
 
 def question_detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'qna/question_detail.html', {'question': question})
+    context = {'question': question}
+    answers = Answer.objects.filter(question=question)
+    if answers:
+        context['answer'] = answers.first()
+    return render(request, 'qna/question_detail.html', context)
+
+
+@login_required
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            return redirect('qna:question_list')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'qna/question_form.html', context)
+
+
+@login_required
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('qna:question_detail', question_id=question.pk)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            petition = form.save(commit=False)
+            petition.modify_date = timezone.now()
+            petition.save()
+            return redirect('qna:question_detail', question_id=question.pk)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'qna/question_form.html', context)
+
+
+@login_required
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('qna:question_detail', question_id=question.id)
+    question.delete()
+    return redirect('qna:question_list')
+
+
+
+# @superuser_required
+# def answer_create(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     if Answer.objects.filter(question=question):
+#         messages.error(request, '이미 답변한 질문 입니다.')
+#         return redirect('qna:question_detail', question_id=question.pk)
+#     if request.method == "POST":
+#         form = AnswerForm(request.POST)
+#         if form.is_valid():
+#             answer = form.save(commit=False)
+#             answer.author = request.user
+#             answer.question = question
+#             answer.save()
+#             return redirect('qna:question_detail', question_id=question.pk)
+#     else:
+#         form = AnswerForm()
+#     content = '답변 등록'
+#     if type == 'reject':
+#         content = '반려 이유'
+#     return render(request, 'petitions/form.html', {'form': form, 'content': content, 'petition_id': petition_id})
