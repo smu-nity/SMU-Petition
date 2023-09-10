54b99fac816d3ae811772833cde0399e615c2aa7
hyunmin0317 <choihm9903@naver.com>
2023-09-10 13:17:30 +0900

    feat: 관리자 페이지 수정 (#260)

--- petitions/admin.py
+++ petitions/admin.py
@@ -4,17 +4,17 @@ from petitions.models import Petition, Comment, Answer
 
 class PetitionAdmin(admin.ModelAdmin):
     search_fields = ['subject', 'content']
-    list_display = ['subject', 'status', 'category', 'content', 'author', 'anonymous', 'create_date', 'modify_date', 'end_date']
+    list_display = ['subject', 'status', 'category', 'author', 'anonymous', 'create_date', 'modify_date', 'end_date']
 
 
 class CommentAdmin(admin.ModelAdmin):
     search_fields = ['petition__subject']
-    list_display = ['author', 'petition', 'content', 'create_date', 'modify_date']
+    list_display = ['petition', 'content', 'author', 'create_date', 'modify_date']
 
 
 class AnswerAdmin(admin.ModelAdmin):
     search_fields = ['petition__subject']
-    list_display = ['author', 'petition', 'content', 'create_date', 'modify_date']
+    list_display = ['petition', 'author', 'create_date', 'modify_date']
 
 
 admin.site.register(Petition, PetitionAdmin)
